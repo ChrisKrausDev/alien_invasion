a87961f2094ce8032bfe1ddf31a8345e53aeb07c
Chris <chris.kraus.dev@gmail.com>
2020-09-22 22:14:40 +0200

    Make them move 266

--- alien_invasion.py
+++ alien_invasion.py
@@ -37,6 +37,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
     def _create_fleet(self):
@@ -77,6 +78,10 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
 
     def _check_events(self):
         """Respond to the keypresses and mouse events."""
