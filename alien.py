fb245bfe1e6db7c2d931fd385cd74b47450994ff
Chris <chris.kraus.dev@gmail.com>
2020-09-23 17:34:01 +0200

    It's moving!

--- alien.py
+++ alien.py
@@ -26,7 +26,7 @@ class Alien(Sprite):
         """Return True if alien is at edge of screen."""
         screen_rect = self.screen.get_rect()
 
-        if self.rect.right >= screen_rect.right or self.rect.left >= 0:
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
             return True
 
     def update(self):
@@ -34,4 +34,3 @@ class Alien(Sprite):
         self.x += (self.settings.alien_speed *
                    self.settings.fleet_direction)
         self.rect.x = self.x
-
